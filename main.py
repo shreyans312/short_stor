da1ca77687aec5ade6b1cc47a704a43f6e8375ac
shreyans312 <43997466+shreyans312@users.noreply.github.com>
2019-01-06 14:21:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-story_title = "the foolish donkey"
+character_name = input("enter the name of the character : ")
+story_title = "the foolish " + character_name
 print(story_title.upper().title())
-character_name = "Donkey"
 print("A salt seller used to carry the salt bag on his " + character_name + " to the market every day.")
 print("On the way they had to cross a stream. One day the " + character_name + " suddenly tumbled down the stream and the salt bag also fell into the water. The salt dissolved in the water and hence the bag became very light to carry. The " + character_name + " was happy.")
 print("Then the " + character_name + " started to play the same trick every day.")
